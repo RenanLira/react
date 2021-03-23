0c82b2d940f0922f78ec97dbb4d8c665107915ae
RenanLira <48999721+RenanLira@users.noreply.github.com>
2021-03-23 19:35:16 -0300

    Add files via upload

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const HelloWorld = (props) =>{
+    return (
+        <div className="main">
+            <h1>
+                Hello World
+            </h1>
+            <p>
+                Programação para Internet 1
+            </p>
+        </div>
+    )
+}
+
+
+export default HelloWorld
